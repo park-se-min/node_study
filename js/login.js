3b0117568e7be0f515aa76e0ae09f3bcd1f46ef9
se-min <discount0414@gmail.com>
2020-08-31 18:51:54 +0900

    2020.08.31 - p169

--- js/login.js
+++ js/login.js
@@ -1,10 +1,22 @@
+// 기본모듈
 var express = require('express');
 var http = require('http');
 var path = require('path');
+
+// 미들웨어
+var bodyParser = require('body-parser');
 var static = require('serve-static');
 
+// 익스프레스 객체
 var app = express();
+
+// 기본설정
 app.set('port', process.env.PORT || 3000);
+
+// body-parser 사용 파싱
+app.use(bodyParser.urlencoded({ extended : false }));
+
+app.use(bodyParser.json());
 
 app.use('/public', static(path.join(__dirname, '../public')));
 
@@ -14,38 +26,14 @@ app.use(function (req, res, next) {
 	console.log('');
 	console.log('1');
 
-	var userAgent = req.header('User-Agent');
-	var paramName = req.query.name;
+	var p_id = req.body.id || req.query.id;
+	var p_pw = req.body.password || req.query.password;
 
-
-	console.log(userAgent);
-	console.log(paramName);
-
-	// res.sendStatus('403');
-
-	// res.send({
-	// 	name: '소녀',
-	// 	age: 20
-	// });
-
-	// req.user = 'ddd';
-
-	// next();
-
+	res.writeHead(200, {'Content-Type':'text/html; charser=utf8'});
+	res.write('<br>id'+ p_id);
+	res.write('<br>pw'+ p_pw);
 	res.end();
 })
-
-// // 2
-// app.use('/', function (req, res, next) {
-// 	console.log('2');
-
-// 	res.writeHead('200', {
-// 		'content-type': 'text/html;charset=utf8'
-// 	});
-// 	res.end('end' + req.user);
-// 	console.log('---');
-// })
-
 
 
 http.createServer(app).listen(app.get('port'), function () {
