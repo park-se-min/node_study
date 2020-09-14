b6e7eabe28cb3dd88701bbbe2feed44703af4296
se-min <discount0414@gmail.com>
2020-09-14 15:24:17 +0900

    2020.09.14 - p257

--- js/virtual_test.js
+++ js/virtual_test.js
@@ -21,6 +21,9 @@ var MongoClient = require('mongodb').MongoClient;
 // 몽고디비 모듈
 var mongoose = require('mongoose');
 
+// crypto 모듈
+var crypto = require('crypto');
+
 // DB 변수들
 var database;
 var UserSchema;
@@ -82,7 +85,7 @@ function connectDB() {
 
 		createUserSchema();
 
-		doTest();
+		// doTest();
 		// console.log('model');
 	});
 
@@ -97,7 +100,17 @@ function createUserSchema() {
 		id: {
 			type: String,
 			required: true,
-			unique: true
+			unique: true,
+			'default': ''
+		},
+		hashed_password: {
+			type: String,
+			required: true,
+			'default': ''
+		},
+		salt: {
+			type: String,
+			required: true
 		},
 		name: {
 			type: String,
@@ -125,23 +138,70 @@ function createUserSchema() {
 	});
 
 	UserSchema
-		.virtual('info')
-		.set(function (info) {
-			var splitted = info.split(' ');
-			this.id = splitted[0];
-			this.name = splitted[1];
-			console.log('설정함 : %s, %s', this.id, this.name);
+		.virtual('password')
+		.set(function (password) {
+			this._password = password;
+			this.salt = this.makeSalt();
+			this.hashed_password = this.encryptPassword(password);
+			console.log('설정함 : %s', this.hashed_password);
+		})
+		.get(function () {
+			return this._password;
 		})
 
+	UserSchema.static('findById', function (id, callback) {
+		return this.find({
+			id: id
+		}, callback);
+	});
+
+	UserSchema.static('findAll', function (callback) {
+		return this.find({}, callback);
+	});
+
+	UserSchema.method('encryptPassword', function (plainText, inSalt) {
+		if (inSalt) {
+			return crypto.createHmac('sha1', inSalt).update(plainText).digest('hex');
+		} else {
+			return crypto.createHmac('sha1', this.salt).update(plainText).digest('hex');
+		}
+	});
+
+	UserSchema.method('makeSalt', function () {
+		return Math.round((new Date().valueOf() * Math.random())) + '';
+	});
+
+	UserSchema.method('authenticate', function (plainText, inSalt, hashed_password) {
+		if (inSalt) {
+			console.log('authenticate 호출 %s -> %S : %S', plainText, this.encryptPassword(plainText, inSalt), hashed_password);
+
+			return this.encryptPassword(plainText, inSalt) === hashed_password;
+		} else {
+			console.log('authenticate 호출 %s -> %S : %S', plainText, this.encryptPassword(plainText), this.hashed_password);
+
+			return this.encryptPassword(plainText) === hashed_password;
+		}
+	});
+
+	UserSchema.path('id').validate(function (id) {
+		return id.length;
+	}, 'id 칼럼의 값');
+
+	UserSchema.path('name').validate(function (name) {
+		return name.length;
+	}, 'name 칼럼의 값');
 
 	console.log('UserSchema 정의함');
 
-	UserModel = mongoose.model('users4', UserSchema);
+	UserModel = mongoose.model('users5_password', UserSchema);
 }
+
+
+
 
 function doTest() {
 	var user = new UserModel({
-		'info': 'test01 소녀'
+		'info': 'test03 소녀'
 	});
 
 	user.save(function (err) {
@@ -153,7 +213,7 @@ function doTest() {
 }
 
 function findAll() {
-	UserModel.finde({}, function (err, reuslts) {
+	UserModel.find({}, function (err, reuslts) {
 		if (err) {
 			throw err;
 		}
@@ -162,6 +222,224 @@ function findAll() {
 		}
 	})
 }
+
+var addUser = function (database, p_arr, callback) {
+	console.log('addUser 호출');
+
+	var {
+		id,
+		password,
+		name
+	} = p_arr;
+
+	console.log(p_arr);
+
+	var p_param = {
+		'id': id,
+		'password': password,
+		'name': name
+	}
+
+	var user = new UserModel(p_param);
+
+	user.save(function (err) {
+		if (err) {
+			callback(err, null);
+			return;
+		}
+
+		console.log('사용자 추가됨');
+		callback(null, user);
+	})
+}
+
+var authUser = function (database, p_arr, callback) {
+	console.log('authUser 호출');
+
+	var {
+		id,
+		password
+	} = p_arr;
+
+	UserModel.findById(id, function (err, results) {
+		if (err) {
+			callback(err, null);
+			return;
+		}
+
+		console.log('---------');
+		console.dir(results);
+		console.log('---------');
+
+		if (results.length > 0) {
+			console.log('아이디 찾음');
+
+			var user = new UserModel({
+				id: id
+			});
+
+			var authenticated = user.authenticate(password, results[0]._doc.salt, results[0]._doc.hashed_password);
+
+			if (authenticated) {
+				console.log('비밀번호 찾음');
+				callback(null, results);
+			} else {
+				console.log('비밀번호 XXXXXXXX');
+				callback(null, null);
+			}
+		} else {
+			console.log('XXXXXXXXX');
+			callback(null, null);
+		}
+	})
+}
+
+router.route('/process/adduser').post(function (req, res) {
+	// 회원가입
+	console.log('/process/adduser');
+
+	var p_id = req.body.id || req.query.id;
+	var p_pw = req.body.password || req.query.password;
+	var p_name = req.body.name || req.query.name;
+
+	var p_arr = {
+		'id': p_id,
+		'password': p_pw,
+		'name': p_name
+	}
+
+	if (database) {
+		// addUser(database, p_id, p_pw, p_name, function (err, result) {
+		addUser(database, p_arr, function (err, result) {
+			if (err) throw err;
+
+			if (result) {
+				console.log(result);
+
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('<br>추가 성공 ');
+				res.write('<br>다시 로그인하기 : <a href="/login_mongo.html">gogo</a>');
+				res.end();
+			} else {
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('<br>실패 ');
+				res.write('<br>다시 로그인하기 : <a href="/login_mongo.html">gogo</a>');
+				res.end();
+			}
+		})
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=utf8'
+		});
+		res.write('<br>DB 연결실패 ');
+		res.end();
+	}
+})
+
+router.route('/process/listuser').post(function (req, res) {
+	console.log('/process/listuser');
+
+	if (database){
+		UserModel.findAll(function(err, results){
+			if (err) {
+				console.log('사용자 리스트 오류');
+
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('<br>사용자 리스트 조회 오류 ');
+				res.end();
+
+				return;
+			}
+
+			if (results){
+				console.dir(results);
+
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('사용자 리스트');
+				res.write('<div><ul>');
+
+				for (let i = 0; i < results.length; i++) {
+					var curId = results[i]._doc.id;
+					var curName = results[i]._doc.name;
+					res.write('<li>#'+ i +' : '+ curId +' : '+ curName +'</li>')
+				}
+
+				res.write('</ul></div>');
+				res.end();
+			} else {
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('실패 ');
+				res.end();
+			}
+		})
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=utf8'
+		});
+		res.write('<br>DB 연결실패 ');
+		res.end();
+	}
+})
+
+router.route('/process/login').post(function (req, res) {
+	console.log('');
+	console.log('/process/login');
+});
+
+// 로그인
+app.post('/process/login', function (req, res) {
+	console.log('/process/login');
+
+	var p_id = req.param('id');
+	var p_pw = req.param('password');
+
+	var p_arr = {
+		'id': p_id,
+		'password': p_pw
+	}
+
+	if (database) {
+		authUser(database, p_arr, function (err, docs) {
+			if (err) throw err;
+
+			if (docs) {
+				console.log(docs);
+				var username = docs[0].name;
+
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('<br>아이디 : ' + p_id);
+				res.write('<br>이름 : ' + username);
+				res.write('<br>다시 로그인하기 : <a href="/login_mongo.html">gogo</a>');
+				res.end();
+			} else {
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf8'
+				});
+				res.write('<br>실패 ');
+				res.write('<br>다시 로그인하기 : <a href="/login_mongo.html">gogo</a>');
+				res.end();
+			}
+		})
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=utf8'
+		});
+		res.write('<br>DB 연결실패 ');
+		res.end();
+	}
+})
 
 
 app.use('/', router);
